e8090360f76ebdfb4d972940c6e09f879e8da973
Tim Van Dyck <tim.van.dyck@davinsi.com>
2026-01-01 18:03:22 +0100

    feat: add semaphore url

--- homelab/providers.tf
+++ homelab/providers.tf
@@ -10,6 +10,7 @@ terraform {
   backend "s3" {
     bucket                      = "tofu-backend"
     key                         = "terraform.tfstate"
+    # key                         = "env:/homelab/homelab/semaphore/terraform.tfstate"
     region                      = "main" # this is required, but will be skipped!
     skip_credentials_validation = true   # this will skip AWS related validation
     skip_metadata_api_check     = true
