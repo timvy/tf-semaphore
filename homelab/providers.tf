3fd2f614272ec8049463ffc30679eca545db77e0
Tim Van Dyck <tim.van.dyck@davinsi.com>
2026-01-01 17:35:28 +0100

    feat: Set s3 key to tofu default

--- homelab/providers.tf
+++ homelab/providers.tf
@@ -9,7 +9,7 @@ terraform {
   }
   backend "s3" {
     bucket                      = "tofu-backend"
-    key                         = "homelab/semaphore/terraform.tfstate"
+    key                         = "terraform.tfstate"
     region                      = "main" # this is required, but will be skipped!
     skip_credentials_validation = true   # this will skip AWS related validation
     skip_metadata_api_check     = true
